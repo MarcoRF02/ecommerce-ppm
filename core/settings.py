484383adfb800a571782e954d5b42015480994d9
MarcoRF02 <marco.rf2002@gmail.com>
2024-06-18 17:20:59 +0200

    preparing for the deploy

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 #settings to configure to allow media to work locally, imported os too
